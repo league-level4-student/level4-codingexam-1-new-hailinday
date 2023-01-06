53991ddb3dd171b9ba98590ac756c4699a111736
league <league@imac-14.attlocal.net>
2023-01-05 18:30:41 -0800

    test

--- src/scheduler/Scheduler.java
+++ src/scheduler/Scheduler.java
@@ -1,5 +1,9 @@
 package scheduler;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 /*
  * Objective: Create a weekly scheduling application.
  * 
@@ -24,8 +28,63 @@ package scheduler;
  *   variables.
  */
 public class Scheduler {
-
+	static LinkedList<event> mon = new LinkedList<event>();
+	static LinkedList<event> teu = new LinkedList<event>();
+	static LinkedList<event> wed = new LinkedList<event>();
+	static LinkedList<event> thurs = new LinkedList<event>();
+	static LinkedList<event> fri = new LinkedList<event>();
+	static LinkedList<event> sat = new LinkedList<event>();
+	static LinkedList<event> sun = new LinkedList<event>();
+	static days[]wholeWeek;
     public static void main(String[] args) {
-
+    	Scanner kb = new Scanner(System.in);
+    	wholeWeek = days.values();
+    	System.out.println("If you want to add an event enter A, to remove enter B");
+    	String choice = kb.nextLine();
+    	if (choice.equals("A")) {
+    		System.out.println("What do you want the event to say?");
+    		String eventText = kb.nextLine();
+    		System.out.println("What time do you want the even to be?");
+    		String eventTime = kb.nextLine();
+    		System.out.println("What day do you want the even on");
+    		String day = kb.nextLine();
+    		event added = new event(eventText,eventTime);
+    		addEvent(added,day);
+    	} else {
+    		System.out.println("What time is the event you want to remove?");
+    		String remTime = kb.nextLine();
+    		System.out.println("What day is the event you want to remove?");
+    		String day = kb.nextLine();
+    		//deleteMethod not added yet
+    	}
+    }
+    public enum days {
+    	
+    	MONDAY(mon), TEUSDAY(teu), WEDNESDAY(wed),THURSDAY(thurs),FRIDAY(fri),SATURDAY(sat),SUNDAY(sun);
+    	LinkedList<event> events;
+		days(LinkedList<event> events) {
+			this.events = events;
+		}
+		public LinkedList<event> getEvents () {
+			return this.events;
+		}
+    	
+    }
+    public static void addEvent(event x, String day) {
+    	if (day.equals("Monday")) {
+			wholeWeek[0].events.add(x);
+		} else if(day.equals("Teusday")) {
+			wholeWeek[1].events.add(x);
+		}else if(day.equals("Wednesday")) {
+			wholeWeek[2].events.add(x);
+		}else if(day.equals("Thursday")) {
+			wholeWeek[3].events.add(x);
+		}else if(day.equals("Friday")) {
+			wholeWeek[4].events.add(x);
+		} else if(day.equals("Saturday")) {
+			wholeWeek[5].events.add(x);
+		} else {
+			wholeWeek[6].events.add(x);
+		}
     }
 }
---
+++ src/scheduler/event.java
@@ -0,0 +1,20 @@
+package scheduler;
+
+public class event {
+	String day;
+	String event;
+	public event (String day, String event) {
+		this.day = day;
+		this.event = event;
+	}
+	public static void main(String[] args) {
+	
+}
+	public String getEvent() {
+		return day;
+		
+	}
+	public void setEvent() {
+		
+	}
+}
